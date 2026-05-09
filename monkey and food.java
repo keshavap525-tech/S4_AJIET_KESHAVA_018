ea86a32228b1944d9ce2a864e8b747bf1593e86c
keshavap525-tech <keshavap525@gmail.com>
2026-05-09 10:07:48 +0530

    Add main class to calculate monkeys on tree

---
+++ monkey and food.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); 
+        int k = sc.nextInt(); 
+        int j = sc.nextInt(); 
+        int m = sc.nextInt(); 
+        int p = sc.nextInt(); 
+        if (n < 0 || k <= 0 || j <= 0 || m < 0 || p < 0) {
+            System.out.println("INVALID INPUT");
+            return;
+        }
+        int bananaMonkeys = m / k;
+        if (m % k != 0) {
+            bananaMonkeys++;
+        }
+        int peanutMonkeys = p / j;
+        if (p % j != 0) {
+            peanutMonkeys++;
+        }
+        int remaining = n - bananaMonkeys - peanutMonkeys;
+        if (remaining < 0) {
+            remaining = 0;
+        }
+
+        System.out.println("Number of Monkeys left on the Tree: " + remaining);
+    }
+}
