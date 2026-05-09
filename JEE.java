f8ad12ad786ff20c32d14d46b4362d4592bfd061
keshavap525-tech <keshavap525@gmail.com>
2026-05-09 13:56:50 +0530

    Add Java program to find and count smallest element
    Implement a Java program to count the number of times the smallest element in an array is updated.

---
+++ JEE.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        for(int i = 0; i < n; i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+
+        int count = 0;
+
+        int smallest = arr[0];
+
+        for(int i = 1; i < n; i++)
+        {
+            if(arr[i] < smallest)
+            {
+                count++;
+                smallest = arr[i];
+            }
+        }
+
+        System.out.println(count);
+    }
+}
